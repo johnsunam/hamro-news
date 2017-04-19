aac3b592ca171c8bb0ee6743499efd8922e468e3
johnsunam <sunamjohn@gmail.com>
2017-04-19 12:45:46 +0545

    models

---
+++ common/models/Tags.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tags".
+ *
+ * @property integer $id
+ * @property string $tag
+ */
+class Tags extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tags';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['tag'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'tag' => 'Tag',
+        ];
+    }
+}
